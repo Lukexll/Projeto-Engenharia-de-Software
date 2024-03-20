80c3428dd1600c3cb481b733867c106e2e097cda
Lukexll <felipewilliam843@gmail.com>
2024-03-20 13:51:09 -0300

    Squashed commit of the following:
    commit 2d8c0f71990d1b4267b1622cb5c924798e7147e2
    Merge: 3a8aa6b 6c048b3
    Author: Felipe William Silva Paulino <54473148+Lukexll@users.noreply.github.com>
    Date:   Wed Mar 20 10:09:28 2024 -0300
    
        Merge pull request #4 from Lukexll/Jhonnathan
    
        Atualizando post
    
    commit 6c048b3837ec1f4516530cd906ee4a78bd15a5f0
    Author: Jhonnathan Jhonny <104373339+Jhonnathan-Jhonny@users.noreply.github.com>
    Date:   Wed Mar 20 07:27:27 2024 -0300
    
        Atualizando post
    
    commit 3a8aa6bfcd6a8ff64d7b1dcc15806201a4660d97
    Merge: c8f5aab 3446145
    Author: Felipe William Silva Paulino <54473148+Lukexll@users.noreply.github.com>
    Date:   Tue Mar 19 19:26:30 2024 -0300
    
        Merge pull request #3 from Lukexll/Felipe
    
        Update UserController.java
    
    commit c8f5aab11e845bddb086a63b3dced52638c378be
    Merge: efadc43 560bf4a
    Author: Felipe William Silva Paulino <54473148+Lukexll@users.noreply.github.com>
    Date:   Tue Mar 19 16:50:56 2024 -0300
    
        Merge pull request #2 from Lukexll:Felipe
    
        DIAGRAMA DE TECNOLOGIAS

--- BackEnd/duckDeliveryAPI/src/main/java/com/api/duckDelivery/dtos/UserDto.java
+++ BackEnd/duckDeliveryAPI/src/main/java/com/api/duckDelivery/dtos/UserDto.java
@@ -1,6 +1,36 @@
 package com.api.duckDelivery.dtos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import java.io.Serializable;
+import java.util.UUID;
+
 //Usado para garantir que os dados do cliente está como desejado.
 //Utilizando notações e nessa parte so está os dados que o cliente irá inserir.
-public class UserDto {
+
+
+@Getter
+@Setter
+public class UserDto implements Serializable {
+
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String nome;
+    @NotBlank
+    @Size(max = 11)
+    private String telefone;
+    @NotBlank
+    private String bairro;
+    @NotBlank
+    private String rua;
+    @NotBlank
+    private String numero;
+    @NotBlank
+    private String senha;
 }
