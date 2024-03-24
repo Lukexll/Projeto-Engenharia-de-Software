7bc31412efc72727032c99d86573bb36c3368194
Lukexll <felipewilliam843@gmail.com>
2024-03-23 21:31:07 -0300

    Atualização da integração
    Descobrimos que podiamos apanhar mais para o react, mas vecemos a guerra.

--- BackEnd/duckDeliveryAPI/src/main/java/com/api/duckDelivery/controllers/UserController.java
+++ BackEnd/duckDeliveryAPI/src/main/java/com/api/duckDelivery/controllers/UserController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 
 
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.duckDelivery.models.LoginModel;
+import com.api.duckDelivery.models.ResponseModel;
 import com.api.duckDelivery.models.UserModel;
 import com.api.duckDelivery.services.CookieService;
 import com.api.duckDelivery.services.UserService;
@@ -34,11 +36,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 public class UserController {
     private final com.api.duckDelivery.services.UserService userService;
+    
 
     public UserController(UserService us, UserService userService) {
         this.userService = userService;
     }
 
+    @Autowired
+    public ResponseModel responseModel;
 
     @PostMapping("/userRegister")
     public ResponseEntity<Object> userRegister(@RequestBody @Valid UserDto userDto){
@@ -53,16 +58,18 @@ public class UserController {
 
     
     @PostMapping("/userLogin")
-    public ResponseEntity<Object> UserLogin(@RequestBody @Valid LoginModel loginParam, HttpServletResponse response, HttpServletRequest requestCookie) {
+    public ResponseEntity<?> UserLogin(@RequestBody @Valid LoginModel loginParam, HttpServletResponse response, HttpServletRequest requestCookie) {
         if(!userService.existsByEmail(loginParam.getEmail())){
-
-            return new ResponseEntity<Object>("Deu ruim", HttpStatus.BAD_REQUEST);
+            responseModel.setMessage("Email não cadastrado.");
+            return new ResponseEntity<ResponseModel>(responseModel,HttpStatus.BAD_REQUEST);
         }
         if(userService.UserLogin(loginParam.getEmail(), loginParam.getSenha()) == null){
-            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflict: Senha incorreta.");
+            responseModel.setMessage("Senha incorreta.");
+            return new ResponseEntity<ResponseModel>(responseModel,HttpStatus.BAD_REQUEST);
         }
         CookieService.setCookie(response, "userId", String.valueOf(userService.findByEmail(loginParam.getEmail()).getId()), 10);
-        return ResponseEntity.status(HttpStatus.ACCEPTED).body("Login efetuado.");
+        responseModel.setMessage("Login efetuado.");
+        return new ResponseEntity<ResponseModel>(responseModel,HttpStatus.ACCEPTED);
             
     } 
     
